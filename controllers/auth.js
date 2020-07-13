492394c0f3f6ba09613f87dfcec990757e286201
Marat <mskabylov@gmail.com>
2020-07-13 17:04:31 +0500

    создание пользователя

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
+
 module.exports.login = function(req, res){
     res.status(200).json({
         login: {
@@ -7,8 +11,29 @@ module.exports.login = function(req, res){
     })
 }
 
-module.exports.register = function(req, res) {
-    res.status(200).json({
-        register: 'from controller'
-    })
+module.exports.register = async function(req, res) {
+    // email password
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        //Пользователь существует, нужно отправить ошибку
+        res.status(409).json({
+            message: 'Такой email уже существует'
+        })
+    } else {
+        // Нужно создать пользователся
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e) {
+            //обработать ошибку
+            
+        }
+    }
 }
